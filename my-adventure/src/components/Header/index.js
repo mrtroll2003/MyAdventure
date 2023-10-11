76ae04f61998d4fbfc1df4bb836c333ecaf80fc4
thinhnhp21520465 <21520465@gm.uit.edu.vn>
2023-10-11 13:43:11 +0700

    update effects for Header Component

--- my-adventure/src/components/Header/index.js
+++ my-adventure/src/components/Header/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './styles.css'
 import PhoneIcon from '../../assets/icons/phone.png'
+import MailIcon from '../../assets/icons/mail.png'
+import FacebookIcon from '../../assets/icons/facebook.png'
+import InstagramIcon from '../../assets/icons/instagram.png'
+import {motion} from "framer-motion"
 export default class Header extends Component {
   render() {
     return (
       <div className='header'> 
-        <img src={PhoneIcon} className='phoneIcon' alt="PhoneIcon.png"/>
-        <p className='text'>(+89) 123456789</p>
-
+        <motion.img src={PhoneIcon} className='phoneIcon' alt="PhoneIcon.png" whileHover={{scale: 1.5}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}}/>
+        <motion.p className='text'  whileHover={{color: 'blue'}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}} >(+89) 123456789</motion.p>
+        <motion.img src={MailIcon} className='mailIcon' alt='MailIcon.png' whileHover={{scale: 1.5}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}}/>
+        <motion.p className='text'  whileHover={{color: 'blue'}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}}>myadvanture@gmail.com</motion.p>
+        <motion.p style={{marginLeft: '25%',}} className='text'>Get to know us</motion.p>
+        <motion.img src={FacebookIcon} className='facebookIcon' alt='FacebookIcon.png' whileHover={{scale: 1.5}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}}/>
+        <motion.img src={InstagramIcon} className='instagramIcon' alt='InstagramIcon.png' whileHover={{scale: 1.5}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}}/>
       </div>
     );
   }
