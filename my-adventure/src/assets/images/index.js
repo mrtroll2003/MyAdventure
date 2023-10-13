1b415d1baa1c45343685fcd08464480e17e67c2f
mrtroll2003 <mrbaopirateengland@gmail.com>
2023-10-13 21:04:28 +0700

    BookingStepOnePage

---
+++ my-adventure/src/assets/images/index.js
@@ -0,0 +1,6 @@
+import Maldives from "./Maldives.png";
+import Morocco from "./Morocco.png";
+import Mongolia from "./Mongolia.png";
+import IntBG from "./InternationalBg.png";
+
+export { Maldives,Mongolia,Morocco,IntBG };
