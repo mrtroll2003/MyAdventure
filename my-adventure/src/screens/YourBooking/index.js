6dbe712daa6aec357621b231776b766419091d4f
mrtroll2003 <mrbaopirateengland@gmail.com>
2023-10-20 16:44:01 +0700

    basic form of YourBooking and Booking component todo: font fix

--- my-adventure/src/screens/YourBooking/index.js
+++ my-adventure/src/screens/YourBooking/index.js
@@ -1,15 +1,29 @@
-import { MyBookingBG } from "../../assets/images";
-
+import React from "react";
+import Header from "../../component/Header";
+import MainTabbar from "../../component/MainTabbar/MainTabbar";
+import data from "./fillData"
+import TourContainer from "../../component/YourBookingContainer";
 import "./styles.css"
 
-const BookingStepOne = () => {
+const YourBooking = () => {
     return (
-      <div>
+      <div className="container">
         <Header></Header>
         <MainTabbar option={"Your Booking"}></MainTabbar>
-        
+        {data.map(booking => (
+        <TourContainer
+          key={booking.id}
+          expectedTime={booking.expectedTime}
+          route={booking.route}
+          numAdult={booking.numAdult}
+          nameA={booking.nameA}
+          numChild={booking.numChild}
+          nameC={booking.nameC}
+          tourStatus={booking.tourStatus}
+        />
+      ))}
       </div>
     );
   };
   
-  export default BookingStepOne;
+  export default YourBooking;
