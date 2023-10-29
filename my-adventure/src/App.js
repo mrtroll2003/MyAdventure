f343ae1cf6025ccb7a6b4c6ff29aeaf7cad0adf8
thinhnhp21520465 <21520465@gm.uit.edu.vn>
2023-10-29 12:58:26 +0700

    Update Booking Management Screen

--- my-adventure/src/App.js
+++ my-adventure/src/App.js
@@ -5,8 +5,10 @@ import HomePageSignedIn from "./screens/HomePageSignedIn";
 import VietNamTourScreen from "./screens/VietNamTourScreen";
 import InternationalTourScreen from "./screens/InternationalTourScreen";
 import YourBooking from "./screens/YourBooking";
+import BookingManagement from "./screens/BookingManagement";
+
 
 function App() {
-  return <InternationalTourScreen />;
+  return <BookingManagement/>;
 }
 export default App;
