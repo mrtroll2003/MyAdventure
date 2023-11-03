f33b57b3b2c20f9a82a5aa14e93cb5b42892423b
thinhnhp21520465 <21520465@gm.uit.edu.vn>
2023-11-03 11:14:07 +0700

    update Header

--- my-adventure/src/component/Header/index.js
+++ my-adventure/src/component/Header/index.js
@@ -10,10 +10,10 @@ export default class Header extends Component {
     return (
       <div className='header'> 
         <motion.img src={PhoneIcon} className='phoneIcon' alt="PhoneIcon.png" whileHover={{scale: 1.5}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}}/>
-        <motion.p className='text'  whileHover={{color: 'blue'}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}} >(+89) 123456789</motion.p>
+        <motion.p className='headerText'  whileHover={{color: 'blue'}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}} >(+89) 123456789</motion.p>
         <motion.img src={MailIcon} className='mailIcon' alt='MailIcon.png' whileHover={{scale: 1.5}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}}/>
-        <motion.p className='text'  whileHover={{color: 'blue'}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}}>myadvanture@gmail.com</motion.p>
-        <motion.p style={{marginLeft: '25%',}} className='text'>Get to know us</motion.p>
+        <motion.p className='headerText'  whileHover={{color: 'blue'}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}}>myadvanture@gmail.com</motion.p>
+        <motion.p style={{marginLeft: '25%',}} className='headerText'>Get to know us</motion.p>
         <motion.img src={FacebookIcon} className='facebookIcon' alt='FacebookIcon.png' whileHover={{scale: 1.5}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}}/>
         <motion.img src={InstagramIcon} className='instagramIcon' alt='InstagramIcon.png' whileHover={{scale: 1.5}} whileTap={{opacity: 0.5, transition:{duration: 0.1}}}/>
       </div>
