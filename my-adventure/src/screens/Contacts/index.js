751b2310150a40ec5653a6313e174f2f4969c01d
mrtroll2003 <mrbaopirateengland@gmail.com>
2023-11-14 10:11:27 +0700

    init

---
+++ my-adventure/src/screens/Contacts/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./styles.module.css";
+import SignedInTabbar from '../../component/SignedInTabbar' //need to add un-signed in tabbar
+import IntroBackground from '../../component/IntroBackground/IntroBackground'
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
