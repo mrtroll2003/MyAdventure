158e7cd88a6323d37c525663370637624ee7ccc3
NguyenHuynhPhucLam <21520316@gm.uit.edu.vn>
2023-11-23 23:03:44 +0700

    Finished: BookingStatusScreen all 4 status and new package

--- my-adventure/src/UpdatedScreens/BookingStatusScreen/BookingStatusScreen.js
+++ my-adventure/src/UpdatedScreens/BookingStatusScreen/BookingStatusScreen.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
+import { FaStar } from "react-icons/fa";
 import { motion } from "framer-motion";
+
+import BirthCert from "../../assets/images/birthcert.png";
+
 import CustomerTextBox from "../../components/CustomerTextBox";
 import { useNavigate } from "react-router-dom";
+import { hover } from "@testing-library/user-event/dist/hover";
 
 const BookingStatusScreen = (props) => {
   const navigate = useNavigate();
+  const [rating, setRating] = useState(null);
+  const [starHover, setStarHover] = useState(null);
   let form = {
     transport: "",
     hotel: "",
@@ -23,7 +30,9 @@ const BookingStatusScreen = (props) => {
     departure_date: "15/11/2023",
     return_date: "20/11/2023",
     booking_date: "05/10/2003",
-    status_code: 0,
+    price: "600",
+    score: rating,
+    status_code: 3,
     status: ["Successful", "Waiting for handling", "Confirmed", "Paid"],
   };
   let statusBackgroundColor;
@@ -34,9 +43,310 @@ const BookingStatusScreen = (props) => {
   } else if (tour.status_code == 2) {
     statusBackgroundColor = "#F5AE45";
   } else if (tour.status_code == 3) {
-    statusBackgroundColor = "#F5AE45";
+    statusBackgroundColor = "#2CF594";
   }
-
+  // display different screen based on status code:
+  let different1, different2;
+  let evidence = {
+    image: BirthCert,
+    name: "giaykhaisinh.jpg",
+  };
+  if (tour.status_code == 0) {
+    // Successful
+    different1 = (
+      <div>
+        <div className={styles.price} style={{ marginTop: "2.5%" }}>
+          Total price: ${tour.price}
+        </div>
+        <div className={styles.text} style={{ fontStyle: "italic" }}>
+          Let’s evaluate for this booking!
+        </div>
+        {/* Star rating section */}
+        <div className={styles.horizon} style={{ marginTop: "2.5%" }}>
+          {[...Array(5)].map((star, index) => {
+            const currentRating = index + 1;
+            return (
+              <label>
+                <input
+                  type="radio"
+                  name="rating"
+                  value={currentRating}
+                  onClick={() => setRating(currentRating)}
+                />
+                <FaStar
+                  className={styles.star}
+                  size={50}
+                  color={
+                    currentRating <= (starHover || rating)
+                      ? "#FFFF00"
+                      : "#e4e5e9"
+                  }
+                  onMouseEnter={() => setStarHover(currentRating)}
+                  onMouseLeave={() => setStarHover(null)}
+                />
+              </label>
+            );
+          })}
+          <motion.button
+            className={styles.submitButton}
+            style={{ marginLeft: "10%" }}
+            whileTap={{ scale: 0.9 }}
+          >
+            submit
+          </motion.button>
+        </div>
+        {/* We gratefully thanks for your evaluation! */}
+        <div className={styles.text} style={{ fontStyle: "italic" }}>
+          We gratefully thanks for your evaluation!
+        </div>
+      </div>
+    );
+    different2 = (
+      <div
+        className={styles.horizon}
+        style={{
+          justifyContent: "space-between",
+          marginTop: "2.5%",
+          marginLeft: "5%",
+          marginRight: "5%",
+        }}
+      >
+        {/* Modify button side */}
+        <div className={styles.halfSide}>
+          <div className={styles.text} style={{ fontWeight: "600" }}>
+            If you want to change above information, <br />
+            please click on “Modify” button!
+          </div>
+          <motion.button
+            className={styles.smallButton}
+            style={{ backgroundColor: "#66F235" }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Modify
+          </motion.button>
+        </div>
+        {/* Cancel button side */}
+        <div className={styles.halfSide}>
+          <div className={styles.text} style={{ fontWeight: "600" }}>
+            If you want to cancel this trip, please click on “Cancel” button!
+          </div>
+          <motion.button
+            className={styles.smallButton}
+            style={{ backgroundColor: "#FF8139" }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Cancel
+          </motion.button>
+        </div>
+      </div>
+    );
+  }
+  if (tour.status_code == 1) {
+    // Waiting for handling
+    different1 = (
+      <div>
+        <div
+          className={styles.price}
+          style={{ marginTop: "2.5%", marginBottom: "5%" }}
+        >
+          Total price: ${tour.price}
+        </div>
+      </div>
+    );
+    different2 = (
+      <div
+        className={styles.horizon}
+        style={{
+          justifyContent: "space-between",
+          marginTop: "2.5%",
+          marginLeft: "5%",
+          marginRight: "5%",
+        }}
+      >
+        {/* Modify button side */}
+        <div className={styles.halfSide}>
+          <div className={styles.text} style={{ fontWeight: "600" }}>
+            If you want to change above information, <br />
+            please click on “Modify” button!
+          </div>
+          <motion.button
+            className={styles.smallButton}
+            style={{ backgroundColor: "#66F235" }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Modify
+          </motion.button>
+        </div>
+        {/* Cancel button side */}
+        <div className={styles.halfSide}>
+          <div className={styles.text} style={{ fontWeight: "600" }}>
+            If you want to cancel this trip, please click on “Cancel” button!
+          </div>
+          <motion.button
+            className={styles.smallButton}
+            style={{ backgroundColor: "#FF8139" }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Cancel
+          </motion.button>
+        </div>
+      </div>
+    );
+  }
+  if (tour.status_code == 2) {
+    // Confirmed
+    different1 = (
+      <div>
+        <div className={styles.price} style={{ marginTop: "2.5%" }}>
+          Total price: ${tour.price}
+        </div>
+        {/* Evidence of your payment for this tour: */}
+        <div className={styles.text} style={{ fontWeight: "600" }}>
+          Evidence of your payment for this tour:
+        </div>
+        <div
+          className={styles.horizon}
+          style={{
+            width: "50%",
+            justifyContent: "space-between",
+            marginTop: "2.5%",
+          }}
+        >
+          <img src={evidence.image} alt={evidence.name} />
+          <div>{evidence.name}</div>
+          <motion.button
+            className={styles.changeButton}
+            whileTap={{ scale: 0.9 }}
+          >
+            Change
+          </motion.button>
+        </div>
+      </div>
+    );
+    different2 = (
+      <div>
+        <div
+          className={styles.horizon}
+          style={{
+            justifyContent: "space-between",
+            marginTop: "2.5%",
+            marginLeft: "5%",
+            marginRight: "5%",
+          }}
+        >
+          {/* Modify button side */}
+          <div className={styles.halfSide}>
+            <div className={styles.text} style={{ fontWeight: "600" }}>
+              If you want to change above information, <br />
+              please click on “Modify” button!
+            </div>
+            <motion.button
+              className={styles.smallButton}
+              style={{ backgroundColor: "#66F235" }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Modify
+            </motion.button>
+          </div>
+          {/* Cancel button side */}
+          <div className={styles.halfSide}>
+            <div className={styles.text} style={{ fontWeight: "600" }}>
+              If you want to cancel this trip, please click on “Cancel” button!
+            </div>
+            <motion.button
+              className={styles.smallButton}
+              style={{ backgroundColor: "#FF8139" }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Cancel
+            </motion.button>
+          </div>
+        </div>
+        {/* Make payment for this booking */}
+        <div
+          className={styles.horizon}
+          style={{
+            marginTop: "7.5%",
+            marginLeft: "5%",
+            marginRight: "5%",
+          }}
+        >
+          <motion.button
+            className={styles.makePaymentButton}
+            whileTap={{ scale: 0.95 }}
+          >
+            Make payment for this booking
+          </motion.button>
+        </div>
+      </div>
+    );
+  }
+  if (tour.status_code == 3) {
+    // Confirmed
+    different1 = (
+      <div>
+        <div className={styles.price} style={{ marginTop: "2.5%" }}>
+          Total price: ${tour.price}
+        </div>
+        {/* Evidence of your payment for this tour: */}
+        <div className={styles.text} style={{ fontWeight: "600" }}>
+          Evidence of your payment for this tour:
+        </div>
+        <div
+          className={styles.horizon}
+          style={{
+            width: "30%",
+            justifyContent: "space-between",
+            marginTop: "2.5%",
+          }}
+        >
+          <img src={evidence.image} alt={evidence.name} />
+          <div>{evidence.name}</div>
+        </div>
+      </div>
+    );
+    different2 = (
+      <div>
+        <div
+          className={styles.horizon}
+          style={{
+            justifyContent: "space-between",
+            marginTop: "2.5%",
+            marginLeft: "5%",
+            marginRight: "5%",
+          }}
+        >
+          {/* Modify button side */}
+          <div className={styles.halfSide}>
+            <div className={styles.text} style={{ fontWeight: "600" }}>
+              If you want to change above information, <br />
+              please click on “Modify” button!
+            </div>
+            <motion.button
+              className={styles.smallButton}
+              style={{ backgroundColor: "#66F235" }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Modify
+            </motion.button>
+          </div>
+          {/* Cancel button side */}
+          <div className={styles.halfSide}>
+            <div className={styles.text} style={{ fontWeight: "600" }}>
+              If you want to cancel this trip, please click on “Cancel” button!
+            </div>
+            <motion.button
+              className={styles.smallButton}
+              style={{ backgroundColor: "#FF8139" }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Cancel
+            </motion.button>
+          </div>
+        </div>
+      </div>
+    );
+  }
   // method:
   const handleTransportChange = (event) => {};
 
@@ -203,7 +513,24 @@ const BookingStatusScreen = (props) => {
             <CustomerTextBox name="Children 2" />
           </div>
         </div>
+        {/* Your additional notion */}
+        <div className={styles.text} style={{ fontWeight: "600" }}>
+          Your additional notion:{" "}
+        </div>
+        {/* Details for this trip */}
+        <div
+          className={styles.text}
+          style={{ fontSize: "4vh", fontStyle: "italic" }}
+        >
+          Details for this trip:{" "}
+          <span className={styles.title} style={{ fontWeight: "800" }}>
+            DETAILS
+          </span>
+        </div>
+        {/* Total price (only display in status_code 0, 1, 2) */}
+        {different1}
       </div>
+      {different2}
     </div>
   );
 };
